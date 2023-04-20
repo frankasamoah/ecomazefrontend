afe945b9abc5f40461f30323c0fda739d63a5de5
Frank Asamoah <frank.asamoah119@gmail.com>
2023-04-20 16:41:00 +0200

    footer alteration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
